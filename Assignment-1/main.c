c8961d9f0ad4ab3e7a5c2110b6ef2fde8025bf8e
Desislava Stoyanova <dstoyanova@Desislavas-MacBook-Air.local>
2015-09-05 11:09:10 +0200

    Initial Commit

---
+++ Assignment-1/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  Assignment-1
+//
+//  Created by Desislava Stoyanova on 9/5/15.
+//  Copyright (c) 2015 Desislava Stoyanova. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
